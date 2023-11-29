101e2fac1ba7d101dd48739dd47a7c05c30330c0
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-11-29 17:23:23 +0100

    Esta es la primera clase de mi Proyecto 3_3

---
+++ Proyecto 3_3/src/Clase3_3/Clase3_3.java
@@ -0,0 +1,12 @@
+package Clase3_3;
+
+public class Clase3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Practica3_3 + Mi nombre");
+		
+	}
+
+}
