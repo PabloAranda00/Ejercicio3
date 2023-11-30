e25956b60b5bab61f3eae6862e6c61815ba45889
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-11-30 17:41:26 +0100

    nuevo cambio

--- Proyecto 3_3/src/Clase3_3/Clase3_3.java
+++ Proyecto 3_3/src/Clase3_3/Clase3_3.java
@@ -5,7 +5,7 @@ public class Clase3_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Practica3_3 + Mi nombre");
+		System.out.println("Canela en rama");
 		
 	}
 
